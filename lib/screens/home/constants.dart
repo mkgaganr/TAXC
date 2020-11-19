dbca6da99ecf2463af215aa80e4ad26c7934ba47
M K GAGAN ROSHAN <53825035+mkgaganr@users.noreply.github.com>
2020-11-19 20:31:58 +0530

    Add files via upload

---
+++ lib/screens/home/constants.dart
@@ -0,0 +1,56 @@
+const FARM_DATA = [
+  {
+    "name":"Coffee",
+    "Tax rate":"five percent",
+    "price":"",
+    "image":"ice_cream.png"
+  },
+  {
+    "name":"Biogas",
+    "Tax rate" : "Five percent",
+    "price":"",
+    "image":"wrap.png"
+  },
+  {
+    "name":"FERTILISERS",
+    "Tax rate":"Five percent",
+    "price":"",
+    "image":"burger.png"
+  },{
+    "name":"Butter",
+    "Tax rate":"12 percent",
+    "price":"",
+    "image":"cheese_dip.png"
+  },
+  {
+    "name":"spoons",
+    "Tax rate":"12 percent",
+    "price":"",
+    "image":"cola.png"
+  },
+  {
+    "name":"Household items",
+    "Tax rate":"18 percent",
+    "price":"",
+    "image":"fries.png"
+  },
+  {
+    "name":"Set up box",
+    "Tax rate":"18 percent",
+    "price":"",
+    "image":"noodles.png"
+  },
+  {
+    "name":"Monitors",
+    "Tax rate":"18 PERCENT",
+    "price":"",
+    "image":"pizza.png"
+  },
+  {
+    "name":"Tobacco",
+    "Tax rate":"28 Percent",
+    "price":"",
+    "image":"sandwich.png"
+  },
+
+];
